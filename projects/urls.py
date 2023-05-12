506baead809f73f4f7a5187f872972b357d51dd6
hugotata78 <hugotatarinoff@gmail.com>
2023-05-12 20:09:41 -0300

    agregando modulo coreapi

--- projects/urls.py
+++ projects/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 from rest_framework.documentation import include_docs_urls
 from .api import ProjectViewSet
 
 router = routers.DefaultRouter()
 
-router.register('api/projects', ProjectViewSet, 'pojects')
+router.register('projects', ProjectViewSet, 'pojects')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("api/v1/", include(router.urls)),
+    path('docs/',include_docs_urls(title='Task API'))
+]
