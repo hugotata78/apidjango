1b97494cc7f107cf92c9bdf2481f22bfe3b839fb
hugotata78 <hugotatarinoff@gmail.com>
2023-05-12 19:55:22 -0300

    reformando urls

--- projects/urls.py
+++ projects/urls.py
@@ -1,13 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework import routers
 from rest_framework.documentation import include_docs_urls
 from .api import ProjectViewSet
 
 router = routers.DefaultRouter()
 
-router.register('projects', ProjectViewSet, 'pojects')
+router.register('api/projects', ProjectViewSet, 'pojects')
 
-urlpatterns = [
-    path("api/v1/", include(router.urls)),
-    path('docs/',include_docs_urls(title='Projects API'))
-]
+urlpatterns = router.urls
